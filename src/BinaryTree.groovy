f1827f766ff19a1a399663a7f5e17aee7aa1bdc0
alex <alexandrosrotsides@gmail.com>
2015-07-20 17:50:01 +0100

    added find node and post order traversal

--- src/BinaryTree.groovy
+++ src/BinaryTree.groovy
@@ -31,11 +31,46 @@ class BinaryTree{
         }
     }
 
+    public MNode findNode(int nodeKey){
+        MNode currentNode = root;
+
+        while( nodeKey != currentNode.key ){
+            if(nodeKey < currentNode.key){
+                currentNode = currentNode.leftNode
+            }
+            if(nodeKey > currentNode.key){
+                currentNode = currentNode.rightNode
+            }
+            if(currentNode == null){ // not found
+                return null
+            }
+        }
+
+        return currentNode
+    }
+
+
     def inOrderTraversal(MNode node, boolean silent){
         if(node){
             inOrderTraversal(node.leftNode, silent)
-            silent == false ?: println("MNode ${node.dump()}")
+            silent == false ?: println("MNode ${node.key}")
             inOrderTraversal(node.rightNode, silent)
+        }
+    }
+
+    def preOrderTraversal(MNode node, boolean silent){
+        if(node){
+            silent == false ?: println("MNode ${node.key}")
+            preOrderTraversal(node.leftNode, silent)
+            preOrderTraversal(node.rightNode, silent)
+        }
+    }
+
+    def postOrderTraversal(MNode node, boolean silent){
+        if(node){
+            postOrderTraversal(node.leftNode, silent)
+            postOrderTraversal(node.rightNode, silent)
+            silent == false ?: println("MNode ${node.key}")
         }
     }
 
--- src/Main.groovy
+++ src/Main.groovy
@@ -1,10 +1,19 @@
 //Test the binaryTree Project
 binaryTree = new BinaryTree();
 
-binaryTree.addNode(45, "v1")
-binaryTree.addNode(60, "v4")
-binaryTree.addNode(12, "v3")
-binaryTree.addNode(32, "v9")
-binaryTree.addNode(415, "v7")
+binaryTree.addNode(50, "fourth")
+binaryTree.addNode(25, "third")
+binaryTree.addNode(15, "second")
+binaryTree.addNode(30, "first")
+binaryTree.addNode(75, "fifth")
+binaryTree.addNode(85, "sixth")
 
+println "In Order Traversal"
 binaryTree.inOrderTraversal(binaryTree.root, true)
+println "Pre Order Traversal"
+binaryTree.preOrderTraversal(binaryTree.root, true)
+println "Post Order Traversal"
+binaryTree.postOrderTraversal(binaryTree.root, true)
+println "Search for 50 and 85"
+println binaryTree.findNode(50).key
+println binaryTree.findNode(85).key
